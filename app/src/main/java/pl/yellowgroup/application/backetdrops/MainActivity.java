8906dd23fa7b65cc3cc07b76b904e6cbc205ad3f
Darek <dariusz@yellowgroup.pl>
2016-10-14 16:12:42 +0200

    - dodanie logo z przyciskiem

--- app/src/main/java/pl/yellowgroup/application/backetdrops/MainActivity.java
+++ app/src/main/java/pl/yellowgroup/application/backetdrops/MainActivity.java
@@ -1,13 +1,22 @@
 package pl.yellowgroup.application.backetdrops;
 
+import android.graphics.Matrix;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.ImageView;
 
 public class MainActivity extends AppCompatActivity {
+    ImageView logo;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        logo = (ImageView) findViewById(R.id.iv_logo);
+
+        Matrix matrix = new Matrix();
+        matrix.setRotate(10.0F);
+        logo.setImageMatrix(matrix);
     }
 }
