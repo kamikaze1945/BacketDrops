5f4d546d9a63586e093c9edb0b047c1b01c81b68
Dariusz Andryskowski <dark82@op.pl>
2016-10-14 23:24:43 +0200

    - dodanie toolbar

--- app/src/main/java/pl/yellowgroup/application/backetdrops/MainActivity.java
+++ app/src/main/java/pl/yellowgroup/application/backetdrops/MainActivity.java
@@ -3,11 +3,13 @@ package pl.yellowgroup.application.backetdrops;
 import android.graphics.Matrix;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
 import android.widget.ImageView;
 
 public class MainActivity extends AppCompatActivity {
 
     ImageView logo;
+    Toolbar mToolbar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -15,9 +17,8 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         logo = (ImageView) findViewById(R.id.iv_logo);
-
-        Matrix matrix = new Matrix();
-        matrix.setRotate(10.0F);
-        logo.setImageMatrix(matrix);
+        mToolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(mToolbar);
     }
+
 }
