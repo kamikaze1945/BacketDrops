2accc4ea19399637de7d4e705dac5fcced3ed313
Darek <dariusz@yellowgroup.pl>
2016-10-21 13:29:26 +0200

    - zapamietanie wybranego filtra, po wyłaczeniu aplikacji

--- app/src/main/java/pl/yellowgroup/application/backetdrops/ActivityMain.java
+++ app/src/main/java/pl/yellowgroup/application/backetdrops/ActivityMain.java
@@ -1,5 +1,6 @@
 package pl.yellowgroup.application.backetdrops;
 
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.LinearLayoutManager;
@@ -22,6 +23,7 @@ import pl.yellowgroup.application.backetdrops.adapters.AdapterDrops;
 import pl.yellowgroup.application.backetdrops.adapters.AddListener;
 import pl.yellowgroup.application.backetdrops.adapters.CompliteListener;
 import pl.yellowgroup.application.backetdrops.adapters.Divider;
+import pl.yellowgroup.application.backetdrops.adapters.Filter;
 import pl.yellowgroup.application.backetdrops.adapters.MarkListener;
 import pl.yellowgroup.application.backetdrops.adapters.SimpleTouchCallback;
 import pl.yellowgroup.application.backetdrops.beans.Drop;
@@ -98,7 +100,11 @@ public class ActivityMain extends AppCompatActivity {
         mBtnAdd = (Button) findViewById(R.id.btn_add);
         mBtnAdd.setOnClickListener(mBtnAddListener);
         mRealm = Realm.getDefaultInstance();
-        mResults = mRealm.where(Drop.class).findAllAsync();
+
+        // check choise sort
+        int filterOption = load();
+        // search in database Realm
+        loadResults(filterOption);
         mEmptyView = findViewById(R.id.empty_drops);
         mRecycler = (BucketRecyclerView) findViewById(R.id.rv_drops);
         mRecycler.addItemDecoration(new Divider(this, LinearLayoutManager.VERTICAL));
@@ -121,30 +127,70 @@ public class ActivityMain extends AppCompatActivity {
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
         int id = item.getItemId();
-
+        boolean handled = true;
+        int filterOption = Filter.NONE;
         switch (id) {
             case R.id.action_add:
                 showDialogAdd();
-                return true;
+                break;
             case R.id.action_sort_ascending_date:
-                mResults = mRealm.where(Drop.class).findAllSortedAsync("when");
-                mResults.addChangeListener(mChangeListener);
-                return true;
+                filterOption = Filter.LEAST_TIME_LEFT;
+                save(Filter.LEAST_TIME_LEFT);
+                break;
             case R.id.action_sort_descending_date:
-                mResults = mRealm.where(Drop.class).findAllSortedAsync("when", Sort.DESCENDING);
-                mResults.addChangeListener(mChangeListener);
-                return true;
+                filterOption = Filter.MOST_TIME_LEFT;
+                save(Filter.MOST_TIME_LEFT);
+                break;
             case R.id.action_show_complete:
-                mResults = mRealm.where(Drop.class).equalTo("completed",true).findAllAsync();
-                mResults.addChangeListener(mChangeListener);
-                return true;
+                filterOption = Filter.COMPLETE;
+                save(Filter.COMPLETE);
+                break;
             case R.id.action_show_incomplete:
-                mResults = mRealm.where(Drop.class).equalTo("completed",false).findAllAsync();
-                mResults.addChangeListener(mChangeListener);
-                return true;
+                filterOption = Filter.INCOMPLETE;
+                save(Filter.INCOMPLETE);
+                break;
+            default:
+                handled = false;
+                break;
         }
+        loadResults(filterOption);
+        return handled;
+    }
 
-        return super.onOptionsItemSelected(item);
+    private void loadResults(int filterOption) {
+        switch (filterOption) {
+            case Filter.NONE:
+                mResults = mRealm.where(Drop.class).findAllAsync();
+                break;
+            case Filter.LEAST_TIME_LEFT:
+                mResults = mRealm.where(Drop.class).findAllSortedAsync("when");
+                break;
+            case Filter.MOST_TIME_LEFT:
+                mResults = mRealm.where(Drop.class).findAllSortedAsync("when", Sort.DESCENDING);
+                break;
+            case Filter.COMPLETE:
+                mResults = mRealm.where(Drop.class).equalTo("completed", true).findAllAsync();
+                break;
+            case Filter.INCOMPLETE:
+                mResults = mRealm.where(Drop.class).equalTo("completed", false).findAllAsync();
+                break;
+        }
+        mResults.addChangeListener(mChangeListener);
+    }
+
+    private void save(int filterOption) {
+        SharedPreferences pref = getPreferences(MODE_PRIVATE);
+        SharedPreferences.Editor editor = pref.edit();
+        editor.putInt("filter", filterOption);
+       // commit() writes the data synchronously (blocking the thread its called from). It then informs you about the success of the operation.
+       // apply() schedules the data to be written asynchronously. It does not inform you about the success of the operation.
+        editor.apply();
+    }
+
+    private int load() {
+        SharedPreferences pref = getPreferences(MODE_PRIVATE);
+        int filterOption = pref.getInt("filter", Filter.NONE);
+        return filterOption;
     }
 
     @Override
---
+++ app/src/main/java/pl/yellowgroup/application/backetdrops/adapters/Filter.java
@@ -0,0 +1,13 @@
+package pl.yellowgroup.application.backetdrops.adapters;
+
+/**
+ * Created by darek on 21.10.2016.
+ */
+
+public interface Filter {
+    int NONE = 0;
+    int MOST_TIME_LEFT = 1;
+    int LEAST_TIME_LEFT = 2;
+    int COMPLETE = 3;
+    int INCOMPLETE = 4;
+}
